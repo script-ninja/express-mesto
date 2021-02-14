43f23fe77789c64f4ac1adb0c1ad62fa2b817121
script-ninja <61618647+script-ninja@users.noreply.github.com>
2021-02-14 13:29:32 +0500

    feat: раздача статики

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const { PORT = 3000 } = process.env;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+app.use(express.static(path.join(__dirname, 'public')));
